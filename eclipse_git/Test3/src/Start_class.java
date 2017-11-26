bf46eee2cde0dcd0a6add69acfc65f7b4267ed63
Stephen <1317stephen@gmail.com>
2017-11-26 23:40:11 +0900

    <RETRY>class AccountMain: main class of Account Book

---
+++ eclipse_git/Test3/src/Start_class.java
@@ -0,0 +1,11 @@
+import java.text.ParseException;
+
+public class Start_class {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		AccountMain am = new AccountMain();
+
+	}
+
+}
