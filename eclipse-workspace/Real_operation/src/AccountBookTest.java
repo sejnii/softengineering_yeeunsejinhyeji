cb4d1274c58c1ba205ecb184870a822a9f57e493
Stephen <1317stephen@gmail.com>
2017-12-23 12:38:10 +0900

    for checkout

--- eclipse-workspace/Real_operation/src/AccountBookTest.java
+++ eclipse-workspace/Real_operation/src/AccountBookTest.java
@@ -1,5 +1,7 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
